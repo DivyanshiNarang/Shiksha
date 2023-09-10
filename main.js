cec867b90934693a7f5a935660629a32206ac36a
Divyanshi <divyanshi.narang16@gmail.com>
2023-09-10 21:34:57 +0530

    Added testimonial section

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0); //immediately on scroll
 });
+
+// show/hide FAQ answers
+
+const faqEl = document.querySelectorAll(".faq");
+
+faqEl.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    // change icon
+    const iconEl = faq.querySelector(".faq__icon i");
+    if (iconEl.className === "uil uil-plus") {
+      iconEl.className = "uil uil-minus";
+      console.log("hi");
+    } else {
+      iconEl.className = "uil uil-plus";
+    }
+  });
+});
