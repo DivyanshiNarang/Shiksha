4e67897bc2df1483ff4d03e7b649727be4f7ba01
Divyanshi <divyanshi.narang16@gmail.com>
2023-09-14 18:04:49 +0530

    Responsive deisgn for mobile and tablet view

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //change navbar styles on scroll
-
 window.addEventListener("scroll", () => {
   document
     .querySelector("nav")
@@ -23,4 +22,21 @@ faqEl.forEach((faq) => {
       iconEl.className = "uil uil-plus";
     }
   });
+});
+
+// show/hide nav hamburger menu
+const menuEl = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menuEl.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
+  menuBtn.style.display = "inline-block";
+  menuEl.style.display = "none";
+  closeBtn.style.display = "none";
 });
